371f5e02dd5d54089fb3a54478051065e395039a
zante85 <nicola.zantedeschi@gmail.com>
2018-01-27 21:04:26 +0100

    pagination lego

--- src/app/container/home/home.component.ts
+++ src/app/container/home/home.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import { LegoService } from '../lego/legoservice';
+import { Lego } from '../lego/lego';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +10,17 @@ import {Component} from '@angular/core';
 })
 
 export class HomeComponent {
-  
+  legos: Lego[];
+  constructor(private legoservice: LegoService){
+
+      this.legoservice.getLegos().subscribe((legos: Array<Lego>) => {
+          console.log(legos);
+          this.legos = legos;
+      });
+
+  }
+
+  changeContainer(name:string){
+    console.log(name);
+  }
 }
--- src/app/container/lego/lego.component.ts
+++ src/app/container/lego/lego.component.ts
@@ -1,7 +1,7 @@
-import {Component, Inject} from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { LegoService } from './legoservice';
 import { Lego } from './lego';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-lego',
@@ -11,16 +11,26 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 export class LegoComponent {
   legos: Lego[];
-  constructor(private legoservice: LegoService){
+  paginations: Number[];
+  constructor(private legoservice: LegoService) {
 
-      this.legoservice.getLegos().subscribe((legos: Array<Lego>) => {
-          console.log(legos);
-          this.legos = legos;
-      });
+    this.legoservice.getLegos().subscribe((legos: Array<Lego>) => {
+      console.log(legos);
+      this.legos = legos;
+      this.paginations = new Array<Number>();
+      let z = 1;
+      // Paginazione 5 elementi per pagina
+      for (var _i = 0; _i < this.legos[0].elements.length; _i++) {
+        if (_i % 5 == 0) {
+          this.paginations[_i] = z;
+          z++;
+        }
+      }
+    });
 
   }
 
-  changeContainer(name:string){
+  changeContainer(name: string) {
     console.log(name);
   }
 }
