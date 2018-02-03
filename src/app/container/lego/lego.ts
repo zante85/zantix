b064808ed7e6eb5993a507cd0cef5d54291aff1f
zante85 <nicola.zantedeschi@gmail.com>
2018-02-03 08:59:29 +0100

    edit lego  json

--- src/app/container/lego/lego.ts
+++ src/app/container/lego/lego.ts
@@ -3,6 +3,8 @@ import { GenericElement } from './genericelement';
 export class Lego {
     id: string;
     title: string;
+    dinizio: string;
+    dfine: string;
     description: string;
     backgroundimage: string;
     icon: string;
