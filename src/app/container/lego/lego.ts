c9b1e3078c68de76fe43e1ea6cbc8155af8746b0
Nicola Zantedeschi <nicola.zantedeschi@gmail.com>
2020-07-08 08:31:40 +0200

    refactor

--- src/app/container/lego/lego.ts
+++ src/app/container/lego/lego.ts
@@ -3,9 +3,11 @@ import { GenericElement } from './genericelement';
 export class Lego {
     id: string;
     title: string;
+    titleLong: string;
     dinizio: string;
     dfine: string;
     description: string;
+    descriptionLong: string;
     backgroundimage: string;
     icon: string;
     text: string;
